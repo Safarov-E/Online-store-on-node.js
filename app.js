28a95048c8e551926272e49a44f5b89ec1fbb9a4
Safarov <elkhan.safarov10@yandex.com>
2020-03-17 11:37:27 +0500

    Препроцессор Pug

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 let express = require('express');
 let app = express();
 app.use(express.static('public'));
+app.set('view engine', 'pug');
 
 app.listen(3000, function(){
     console.log('node express work on 3000');
 });
 
 app.get('/', function(req, res) {
-    res.render('index.html')
+    res.render('main', {
+        foo: 4,
+        bar: 7
+    })
 })
