296af960486be388c672d9845de19e54931bf407
Safarov <elkhan.safarov10@yandex.com>
2020-03-18 14:06:59 +0500

    MySQL Подключаемся и получаем список товаров

--- app.js
+++ app.js
@@ -1,15 +1,37 @@
 let express = require('express');
+let mysql = require('mysql');
 let app = express();
+
 app.use(express.static('public'));
 app.set('view engine', 'pug');
+
+let con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'market'
+})
 
 app.listen(3000, function(){
     console.log('node express work on 3000');
 });
 
 app.get('/', function(req, res) {
-    res.render('main', {
-        foo: 4,
-        bar: 7
-    })
+    con.query(
+        'SELECT * FROM goods',
+        function(error, result) {
+            if(error) throw error
+            // console.log(result)
+            let goods = {};
+            for (let i = 0; i < result.length; i++) {
+                goods[result[i]['id']] = result[i]
+            }
+            console.log(JSON.parse(JSON.stringify(goods)))
+            res.render('main', {
+                foo: 4,
+                bar: 7,
+                goods : JSON.parse(JSON.stringify(goods))
+            })
+        }
+    );
 })
