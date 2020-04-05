4c3de8e0bb7f9ce51b75dbf7427c66769ddcb57d
Safarov <elkhan.safarov10@yandex.com>
2020-04-05 11:50:12 +0500

    Админ панель. Логин

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 let express = require('express');
 let mysql = require('mysql');
 let app = express();
+let cookieParser = require('cookie-parser')
+let admin = require('./admin');
 
 app.use(express.static('public'));
 app.set('view engine', 'pug');
 
 app.use(express.json());
+app.use(express.urlencoded());
+app.use(cookieParser());
+
 const nodemailer = require("nodemailer");
 
 let con = mysql.createConnection({
@@ -19,6 +24,14 @@ process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
 
 app.listen(3000, function(){
     console.log('node express work on 3000');
+});
+
+app.use(function (req, res, next) {
+    if(req.originalUrl == '/admin' || req.originalUrl == '/admin-order') {
+        admin(req, res, con, next)
+    } else {
+        next()
+    }
 });
 
 app.get('/', function(req, res) {
@@ -85,98 +98,136 @@ app.get('/order', function(req, res) {
     res.render('order')
 })
 
-app.post('/get-category-list', function(req, res){
-    con.query('SELECT id, category FROM category', 
-        function(error, result){
-            if(error) throw error;
-            res.json(result)
-        })
-})
+app.post('/get-category-list', function (req, res) {
+    con.query('SELECT id, category FROM category', function (error, result, fields) {
+      if (error) throw error;
+      console.log(result)
+      res.json(result);
+    });
+  });
 
 app.post('/get-goods-info', function (req, res) {
     console.log(req.body.key);
-    if (req.body.key.length !=0){
-      con.query('SELECT id,name,cost FROM goods WHERE id IN ('+req.body.key.join(',')+')', function (error, result, fields) {
+    if (req.body.key.length != 0) {
+      con.query('SELECT id,name,cost FROM goods WHERE id IN (' + req.body.key.join(',') + ')', function (error, result, fields) {
         if (error) throw error;
         console.log(result);
         let goods = {};
-        for (let i = 0; i < result.length; i++){
+        for (let i = 0; i < result.length; i++) {
           goods[result[i]['id']] = result[i];
         }
         res.json(goods);
       });
     }
-    else{
+    else {
       res.send('0');
     }
   });
 
   app.post('/finish-order', function (req, res) {
-    if(req.body.key.length != 0) {
-        con.query('SELECT id,name,cost FROM goods WHERE id IN ('+req.body.key.join(',')+')',
+    console.log(req.body);
+    if (req.body.key.length != 0) {
+      let key = Object.keys(req.body.key);
+      con.query(
+        'SELECT id,name,cost FROM goods WHERE id IN (' + key.join(',') + ')',
         function (error, result, fields) {
-            if (error) throw error;
-            console.log(result);
-            sendMail(req.body, result).catch(console.error)
-            saveOrder(req.body, result)
-            res.send('1')
+          if (error) throw error;
+          console.log(result);
+          sendMail(req.body, result).catch(console.error);
+          saveOrder(req.body, result);
+          res.send('1');
         });
     }
-  })
+    else {
+      res.send('0');
+    }
+  });
 
-app.get('/admin', function(req, res) {
-    res.render('admin', {})
-})
-app.get('/admin-order', function(req, res) {
+  app.get('/admin', function (req, res) {
+    res.render('admin', {});
+  });
+
+app.get('/admin-order', function (req, res) {
     con.query(`SELECT 
-	shop_order.id as id,
-	shop_order.user_id as user_id,
-    shop_order.goods_id as goods_id,
-    shop_order.goods_cost as goods_cost,
-    shop_order.goods_amount as goods_amount,
-    shop_order.total as total,
-    from_unixtime(date,"%Y-%m-%d %h:%m") as human_date,
-    user_info.user_name as user,
-    user_info.user_phone as phone,
-    user_info.address as address
-FROM 
-	shop_order
-LEFT JOIN	
-	user_info
-ON shop_order.user_id = user_info.id ORDER BY id DESC`, 
-        function(error, result){
-            if(error) throw error;
-            res.render('admin-order', {order: JSON.parse(JSON.stringify(result))})
-        })
-})
-app.get('/login', function(req, res) {
-    res.render('login', {})
-})
-app.post('/login', function(req, res) {
+        shop_order.id as id,
+        shop_order.user_id as user_id,
+          shop_order.goods_id as goods_id,
+          shop_order.goods_cost as goods_cost,
+          shop_order.goods_amount as goods_amount,
+          shop_order.total as total,
+          from_unixtime(date,"%Y-%m-%d %h:%m") as human_date,
+          user_info.user_name as user,
+          user_info.user_phone as phone,
+          user_info.address as address
+      FROM 
+        shop_order
+      LEFT JOIN	
+        user_info
+      ON shop_order.user_id = user_info.id ORDER BY id DESC`, function (error, result, fields) {
+        if (error) throw error;
+        console.log(result);
+        res.render('admin-order', { order: JSON.parse(JSON.stringify(result)) });
+      });
+  });
+app.get('/login', function (req, res) {
+    res.render('login', {});
+  });
+  
+  app.post('/login', function (req, res) {
+    console.log('=======================');
+    console.log(req.body);
+    console.log(req.body.login);
+    console.log(req.body.password);
+    console.log('=======================');
     con.query(
-        'SELECT * FROM user WHERE login="' + req.body.login + '" and password="' + req.body.password + '"',
-        function (error, result) {
-          if (error) reject(error);
-          console.log(result);
-          console.log(result.length);
-          if (result.length == 0) {
-            console.log('error user not found');
-            res.redirect('/login');
-          }
-          else {
-            result = JSON.parse(JSON.stringify(result));
-            res.cookie('hash', 'blablabla');
-            sql = "UPDATE user  SET hash='blablabla' WHERE id=" + result[0]['id'];
-            con.query(sql, function (error, resultQuery) {
-              if (error) throw error;
-              res.redirect('/admin');
-            });
-    
-    
-          };
-        });
-})
+      'SELECT * FROM user WHERE login="' + req.body.login + '" and password="' + req.body.password + '"',
+      function (error, result) {
+        if (error) reject(error);
+        console.log(result);
+        console.log(result.length);
+        if (result.length == 0) {
+          console.log('error user not found');
+          res.redirect('/login');
+        }
+        else {
+          result = JSON.parse(JSON.stringify(result));
+          let hash = makeHash(32);
+          res.cookie('hash', hash);
+          res.cookie('id', result[0]['id']);
+          /**
+           * write hash to db
+           */
+          sql = "UPDATE user  SET hash='" + hash + "' WHERE id=" + result[0]['id'];
+          con.query(sql, function (error, resultQuery) {
+            if (error) throw error;
+            res.redirect('/admin');
+          });
+  
+  
+        };
+      });
+  });
 
+function saveOrder(data, result) {
+    let sql;
+    sql = "INSERT INTO user_info (user_name, user_phone, user_email, address) VALUES ('" + data.username + "','" + data.phone + "','" + data.email + "','" + data.address + "')";
+    con.query(sql, function (error, resultQuery) {
+      if (error) throw error;
+      console.log('1 user info saved');
+      console.log(resultQuery);
+      let userId = resultQuery.insertId;
+      date = new Date() / 1000;
+      for (let i = 0; i < result.length; i++) {
+        sql = "INSERT INTO shop_order(date, user_id, goods_id,goods_cost, goods_amount, total) VALUES (" + date + "," + userId + "," + result[i]['id'] + "," + result[i]['cost'] + "," + data.key[result[i]['id']] + "," + data.key[result[i]['id']] * result[i]['cost'] + ")";
+        con.query(sql, function (error, resultQuery) {
+          if (error) throw error;
+          console.log("1 goods saved");
+        })
+      }
+    });
+  
+  }
+  
   async function sendMail(data, result) {
     let res = '<h2>Order in lite shop</h2>';
     let total = 0;
@@ -203,33 +254,27 @@ app.post('/login', function(req, res) {
         pass: testAccount.pass // generated ethereal password
       }
     });
+  
     let mailOption = {
-        from: '<elkhan.safarov10@yandex.com>',
-        to: 'elkhan.safarov10@yandex.com,'+data.email,
-        subject: 'Lite shop order',
-        text: 'Hello world',
-        html: res
-    }
-    let info = await transporter.sendMail(mailOption)
+      from: '<luschenko@gmail.com>',
+      to: "luschenko@gmail.com," + data.email,
+      subject: "Lite shop order",
+      text: 'Hello world',
+      html: res
+    };
+  
+    let info = await transporter.sendMail(mailOption);
+    console.log("MessageSent: %s", info.messageId);
+    console.log("PreviewSent: %s", nodemailer.getTestMessageUrl(info));
     return true;
   }
-
-function saveOrder(data, result) {
-    // data - информация о пользователе
-    // result - сведения о товаре
-    let sql = "INSERT INTO user_info (user_name, user_phone, user_email, address) VALUES ('"+data.username+"','"+data.phone+"','"+data.email+"','"+data.address+"')";
-    con.query(sql, function(error, resultQuery) {
-        if(error) throw error;
-        let userId = resultQuery.insertId;
-        date = new Date()/1000;
-        for(let i = 0; i < result.length; i++) {
-            sql = "INSERT INTO shop_order (data, user_id, goods_id, goods_cost, goods_amount, total) VALUES ("+date+","+userId+","+result[i]['id']+","+result[i]['cost']+data.key[result[i]['id']]+","+data.key[result[i]['id']]*result[i]['cost']+")";
-            con.query(sql, function(error, result) {
-                if(error) throw error;
-            })
-        }
-    })
-}
-async function sendMail(data, result) {
-
-}
+  
+    function makeHash(length) {
+      var result = '';
+      var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+      var charactersLength = characters.length;
+      for (var i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      }
+      return result;
+    }
